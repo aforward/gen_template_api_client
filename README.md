a8e88a5a29546047d4094e4d1acf41ecabd73545
Andrew Forward <aforward@gmail.com>
2017-04-24 11:00:20 -0400

    Initial commit from 'mix gen template gen_template_api_client'

---
+++ README.md
@@ -0,0 +1,19 @@
+# GenTemplateApiClient
+
+> TODO: Add description**
+
+
+## Installation
+
+```elixir
+@deps [
+  gen_template_api_client: "~> 0.1.0"
+]
+```
+
+## License
+
+> TODO: Add license
+
+----
+Created:  2017-04-24Z
---
+++ lib/gen_template_api_client.ex
@@ -0,0 +1,11 @@
+defmodule GenTemplateApiClient do
+
+  @moduledoc File.read!(Path.join([__DIR__, "../README.md"]))
+  
+  use MixTemplates,
+    name:       :gen_template_api_client,
+    short_desc: "Template for ....",
+    source_dir: "../template"
+
+  
+end
