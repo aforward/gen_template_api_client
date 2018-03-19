b0f2a6e0a4e3d70def1c388219d0c57aa0f78010
Andrew Forward <aforward@gmail.com>
2018-03-19 15:39:32 -0400

    Add code formatter

--- lib/gen_template_api_client.ex
+++ lib/gen_template_api_client.ex
@@ -1,25 +1,23 @@
 defmodule Mix.Gen.Template.ApiClient do
-
   @moduledoc File.read!(Path.join([__DIR__, "../README.md"]))
 
   use MixTemplates,
-    name:       :api_client,
+    name: :api_client,
     short_desc: "Template for creating REST API client wrappers in Elixir",
     source_dir: "../template",
-    options:    [
+    options: [
       app: [
-        to:       :app,
+        to: :app,
         required: false,
-        takes:    "app_name",
-        desc:     "sets the application name to «app_name»"
+        takes: "app_name",
+        desc: "sets the application name to «app_name»"
       ],
-      application: [ same_as: :app ],
-
+      application: [same_as: :app],
       module: [
-         to:        :project_name_camel_case,
-         required:  false,
-         takes:     "«project_name»",
-         desc:      "sets the name of the module to «project_name»"
-       ]
+        to: :project_name_camel_case,
+        required: false,
+        takes: "«project_name»",
+        desc: "sets the name of the module to «project_name»"
+      ]
     ]
 end
--- mix.exs
+++ mix.exs
@@ -1,17 +1,17 @@
 defmodule GenTemplateApiClient.Mixfile do
   use Mix.Project
 
-  @name    :gen_template_api_client
+  @name :gen_template_api_client
   @version "0.3.0"
 
   @deps [
-    { :mix_templates,  ">0.0.0",  app: false },
-    { :ex_doc,         ">0.0.0",  only: [:dev, :test] },
-    { :version_tasks,  "~> 0.10"},
+    {:mix_templates, ">0.0.0", app: false},
+    {:ex_doc, ">0.0.0", only: [:dev, :test]},
+    {:version_tasks, "~> 0.10"}
   ]
 
   @maintainers ["Andrew Forward <aforward@gmail.com>"]
-  @github      "https://github.com/aforward/#{@name}"
+  @github "https://github.com/aforward/#{@name}"
 
   @description """
   A template for building API clients to 3rd party REST applications.
@@ -24,34 +24,39 @@ defmodule GenTemplateApiClient.Mixfile do
   # ------------------------------------------------------------
 
   def project do
-    in_production = Mix.env == :prod
+    in_production = Mix.env() == :prod
+
     [
-      app:     @name,
+      app: @name,
       version: @version,
-      deps:    @deps,
-      elixir:  "~> 1.4",
+      deps: @deps,
+      elixir: "~> 1.4",
       package: package(),
-      description:     @description,
-      build_embedded:  in_production,
-      start_permanent: in_production,
+      description: @description,
+      build_embedded: in_production,
+      start_permanent: in_production
     ]
   end
 
   defp package do
     [
-      name:        @name,
-      files:       [
-                      "lib", "mix.exs", "README.md", "LICENSE.md", "template",
-                      "template/$PROJECT_NAME$/.gitignore",
-                      "template/$PROJECT_NAME$/.iex.exs",
-                   ],
+      name: @name,
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md",
+        "LICENSE.md",
+        "template",
+        "template/$PROJECT_NAME$/.gitignore",
+        "template/$PROJECT_NAME$/.formatter.exs",
+        "template/$PROJECT_NAME$/.iex.exs"
+      ],
       maintainers: @maintainers,
-      licenses:    ["Apache 2.0"],
-      links:       %{
-        "GitHub" => @github,
-      },
-#      extra:       %{ "type" => "a_template_for_mix_gen" },
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github
+      }
+      #      extra:       %{ "type" => "a_template_for_mix_gen" },
     ]
   end
-
 end
